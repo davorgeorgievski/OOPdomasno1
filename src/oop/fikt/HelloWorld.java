9b6b80358e06c96daed383820c4a14adaef63454
Davor Georgievski <davor.georgievski@yahoo.com>
2022-10-27 20:49:44 +0200

    Create OOPdomasno1

--- src/oop/fikt/HelloWorld.java
+++ src/oop/fikt/HelloWorld.java
@@ -1,7 +1,7 @@
 package oop.fikt;
 
 public class HelloWorld {
-	public static void main(String[] arcs) {
+	public static void main(String[] args) {
 		System.out.println("Hello World OOP FIKT");
 	}
 }
